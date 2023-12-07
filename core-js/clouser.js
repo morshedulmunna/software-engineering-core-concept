804923b0cb24eef4f0494c5580ac6c24602cbfe6
morshedulmunna <morshedulmunna1@gmail.com>
2023-12-07 12:24:26 +0600

    update:/closure concept

--- core-js/clouser.js
+++ core-js/clouser.js
@@ -1,13 +1,23 @@
-function outer() {
-    let b = 1;
-    function inner() {
-        return b;
-    }
-    return inner;
-}
-let get_func_inner = outer(); //here create closure outer function
+// function outer() {
+//     let b = 1;
+//     function inner() {
+//         return b;
+//     }
+//     return inner;
+// }
+// let get_func_inner = outer(); //here create closure outer function
 
-/* how can access b after returning a outer function. 
-normally it's could not be access b because outer function return 
-then it's not stay in call stack and memory */
-console.log(get_func_inner());
+// /* how can access b after returning a outer function.
+// normally it's could not be access b because outer function return
+// then it's not stay in call stack and memory */
+// console.log(get_func_inner());
+
+function outer() {
+    let a = 20;
+
+    setTimeout(function () {
+        console.log(a);
+    }, 0);
+}
+
+outer();
